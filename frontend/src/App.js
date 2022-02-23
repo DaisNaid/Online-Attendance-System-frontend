b175b5e2ebe46fd1e0eb2524ac11673578b2858a
Tryve <iamtryve@gmail.com>
2022-02-23 18:27:20 +0000

    Enter

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <div className="social-call">
+          <div className="title">
+            <a href="/">OAS</a>
+          </div>
+          <div className="social">
+            <a href="mailto:oas@gmail.com">
+              <i className="fa fa-envelope-o" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </header>
+      <main>Hello</main>
+    </div>
+  );
+}
+
+export default App;
